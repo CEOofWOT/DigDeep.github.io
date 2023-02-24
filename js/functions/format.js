6d882779bed86edeb5c41b49fbda4d77977a4a8d
W.O.T <119104911+CEOofWOT@users.noreply.github.com>
2023-02-24 22:47:43 +0000

    Update format.js

--- js/functions/format.js
+++ js/functions/format.js
@@ -2,19 +2,19 @@ function format(number,displayNumber) {
     if (number == depth || number == dtrm.cost) {
         if (number.gte(1000000) && number.lt(1000000000)) {
             displayNumber = displayNumber.div(1000000);
-            return (ExpantaNum.toStringWithDecimalPlaces(4, displayNumber) + "Mm");
+            return (displayNumber.toPrecise(4) + "Mm");
         } if (number.gte(1000) && number.lt(1000000)) {
             displayNumber = displayNumber.div(1000);
-            return (ExpantaNum.toStringWithDecimalPlaces(4, displayNumber) + "km");
+            return (displayNumber.toPrecise(4) + "km");
         } else {
-            return (ExpantaNum.toStringWithDecimalPlaces(4, displayNumber) + "m");
+            return (displayNumber.toPrecise(4) + "m");
         };
     } else {
         if (number.gte(1000) && number.lt(1000000)) {
             displayNumber = displayNumber.div(1000);
-            return (ExpantaNum.toStringWithDecimalPlaces(4, displayNumber) + "k");
+            return (displayNumber.toPrecise(4) + "k");
         } if (number.gte(0) && number.lt(1000)) {
-            return (ExpantaNum.toStringWithDecimalPlaces(4, displayNumber));
+            return (displayNumber.toPrecise(4));
         };
     };
 };
