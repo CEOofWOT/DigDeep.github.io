fc46e26acd48012304a622fb736ad05625313c0c
W.O.T <119104911+CEOofWOT@users.noreply.github.com>
2023-02-25 20:10:56 +0000

    Update format.js

--- js/functions/format.js
+++ js/functions/format.js
@@ -2,19 +2,19 @@ function format(number,displayNumber) {
     if (number == depth || number == dtrm.cost) {
         if (number.gte(1000000) && number.lt(1000000000)) {
             displayNumber = displayNumber.div(1000000);
-            return (ExpantaNum(displayNumber).toFixed(4) + "Mm");
+            return (Number(ExpantaNum(displayNumber).toFixed(4)) + "Mm");
         } if (number.gte(1000) && number.lt(1000000)) {
             displayNumber = displayNumber.div(1000);
-            return (ExpantaNum(displayNumber).toFixed(4) + "km");
+            return (Number(ExpantaNum(displayNumber).toFixed(4)) + "km");
         } else {
-            return (ExpantaNum(displayNumber).toFixed(4) + "m");
+            return (Number(ExpantaNum(displayNumber).toFixed(4)) + "m");
         };
     } else {
         if (number.gte(1000) && number.lt(1000000)) {
             displayNumber = displayNumber.div(1000);
-            return (ExpantaNum(displayNumber).toFixed(4) + "k");
+            return (Number(ExpantaNum(displayNumber).toFixed(4)) + "k");
         } if (number.gte(0) && number.lt(1000)) {
-            return (ExpantaNum(displayNumber).toFixed(4) + "");
+            return (Number(ExpantaNum(displayNumber).toFixed(4)) + "");
         };
     };
 };
