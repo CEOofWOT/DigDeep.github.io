f1da4756269006218dd8f97a677c6d06741a1d9e
W.O.T <119104911+CEOofWOT@users.noreply.github.com>
2023-02-25 19:56:37 +0000

    Update format.js

--- js/functions/format.js
+++ js/functions/format.js
@@ -7,14 +7,14 @@ function format(number,displayNumber) {
             displayNumber = displayNumber.div(1000);
             return (displayNumber.toPrecise(4) + "km");
         } else {
-            return displayNumber + "m";
+            return (displayNumber.toPrecise(4) + "m");
         };
     } else {
         if (number.gte(1000) && number.lt(1000000)) {
             displayNumber = displayNumber.div(1000);
             return (displayNumber.toPrecise(4) + "k");
         } if (number.gte(0) && number.lt(1000)) {
-            return displayNumber + "";
+            return (displayNumber.toPrecise(4) + "");
         };
     };
 };
